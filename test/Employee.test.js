585351d62ccc1a8c3263b39ff2949f68b27956e8
temptado <jason1287712@gmail.com>
2021-08-12 21:10:28 -0400

    added tests for employee class

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -1,10 +1,11 @@
+const { describe, it, expect } = require('@jest/globals');
 const Employee = require('../lib/Employee');
 
 describe('Employee class', () => {
     describe('Initialization', () => {
 
         it("should return an object containing a 'name' property", () => {
-            const obj = new Employee('jason');
+            const obj = new Employee();
     
             expect("name" in obj).toEqual(true);
         });
@@ -21,7 +22,46 @@ describe('Employee class', () => {
         });
 
     });
+
+    describe("getName", () => {
+
+        it("should return the name property inside the object", () => {
+            const obj = new Employee('json',12,'jason');
+
+            expect(obj.getName()).toBe('json');
+        });
+
+    });
     
+    describe("getId", () => {
+
+        it("should return the id property inside the object", () => {
+            const obj = new Employee('json',12,'jason');
+
+            expect(obj.getID()).toBe(12);
+        });
+
+    });
+
+    describe("getEmail", () => {
+
+        it("should return the email property inside the object", () => {
+            const obj = new Employee('json',12,'jason');
+
+            expect(obj.getEmail()).toBe('jason');
+        });
+
+    });
+
+    describe("getRole", () => {
+
+        it("should return the role of the team member", () => {
+            const obj = new Employee();
+
+            expect(obj.getRole()).toBe('Employee');
+        });
+
+    });
 
 
 });
