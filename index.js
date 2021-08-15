defbe1870528adc414deda0e13157022513b45dc
temptado <jason1287712@gmail.com>
2021-08-15 15:43:57 -0400

    added comments to index.js file

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// Import prompts file in order to use the init function.
 const init = require('./lib/Prompts');
 
+// Calls the functions and the prompts run once the command 'node index.js' is run in the terminal.
 init();
